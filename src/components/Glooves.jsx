26ab4a721323dddb8d2426c506bd03129e9fbb74
Khayam Ijaz <152375599+MrKhayam@users.noreply.github.com>
2024-07-06 08:51:46 +0500

    Update Glooves.jsx

--- src/components/Glooves.jsx
+++ src/components/Glooves.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 const Glooves = () => {
   return (
     <div className='w-[95%] m-auto flex flex-col items-center gap-8'>
-        <Card imgSrc={'/gl1.jpg'} price={'140'} />
-        <Card imgSrc={'/gl2.jpg'} price={'140'} />
-        <Card imgSrc={'/gl3.jpg'} price={'140'} />
-        <Card imgSrc={'/gl4.jpg'} price={'140'} />
+        <Card imgSrc={'/gl1.jpg'} price={'140'} chez={'Glooves'} />
+        <Card imgSrc={'/gl2.jpg'} price={'140'} chez={'Glooves'} />
+        <Card imgSrc={'/gl3.jpg'} price={'140'} chez={'Glooves'} />
+        <Card imgSrc={'/gl4.jpg'} price={'140'} chez={'Glooves'} />
     </div>
   )
 }
