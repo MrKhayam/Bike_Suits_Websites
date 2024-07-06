b2a697eeb49f5e4c76a939a08c70c024a93b1cdd
Khayam Ijaz <152375599+MrKhayam@users.noreply.github.com>
2024-07-06 08:43:30 +0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Card = ({imgSrc,price}) => {
+const Card = ({imgSrc,price,chez}) => {
   return (
     <>
       <div className="w-[90%] rounded-md border-[#0096bb] border h-auto flex flex-col items-center">
         <img className='rounded-t-md' src={imgSrc} alt="" />
         <div className='w-full h-10 bg-gradient-to-r from-[#00617a] via-[#0096bb] to-[#00617a] flex items-center justify-center text-center'>
-            <h1 className='text-white text-lg font-semibold'>€{price}</h1>
+            <h1 className='text-white text-lg font-semibold'>Cowhead Leather {chez} €{price}</h1>
         </div>
       </div>
     </>
