98855c79af565c51773e6dbb8c6fee9f4537a566
Khayam Ijaz <152375599+MrKhayam@users.noreply.github.com>
2024-07-06 08:45:01 +0500

    Update Boots.jsx

--- src/components/Boots.jsx
+++ src/components/Boots.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 const Boots = () => {
   return (
     <div className='w-[95%] m-auto flex flex-col items-center gap-8'>
-        <Card imgSrc={'/bot1.jpg'} price={'170'} />
-        <Card imgSrc={'/bot2.jpg'} price={'170'} />
-        <Card imgSrc={'/bot3.jpg'} price={'170'} />
-        <Card imgSrc={'/bot1.jpg'} price={'170'} />
+        <Card imgSrc={'/bot1.jpg'} price={'170'} chez={'Boots'} />
+        <Card imgSrc={'/bot2.jpg'} price={'170'} chez={'Boots'} />
+        <Card imgSrc={'/bot3.jpg'} price={'170'} chez={'Boots'} />
+        <Card imgSrc={'/bot1.jpg'} price={'170'} chez={'Boots'} />
     </div>
   )
 }
