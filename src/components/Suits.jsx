353c03ef3799912fabf24a853c1d6bfe24ce7aed
Khayam Ijaz <152375599+MrKhayam@users.noreply.github.com>
2024-07-06 08:50:30 +0500

    Update Suits.jsx

--- src/components/Suits.jsx
+++ src/components/Suits.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 const Suits = () => {
   return (
     <div className='w-[95%] m-auto flex flex-col items-center gap-8'>
-        <Card imgSrc={'/main1.jpg'} price={'500'} />
-        <Card imgSrc={'/main2.jpg'} price={'450'} />
-        <Card imgSrc={'/main3.jpg'} price={'480'} />
-        <Card imgSrc={'/main4.jpg'} price={'480'} />
+        <Card imgSrc={'/main1.jpg'} price={'500'} chez={'Suit'} />
+        <Card imgSrc={'/main2.jpg'} price={'450'} chez={'Suit'} />
+        <Card imgSrc={'/main3.jpg'} price={'480'} chez={'Suit'} />
+        <Card imgSrc={'/main4.jpg'} price={'480'} chez={'Suit'} />
     </div>
   )
 }
