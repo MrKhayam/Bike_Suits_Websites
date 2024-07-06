f316bb983209632dc10bc0ed584afdfb042d5cfc
Khayam Ijaz <152375599+MrKhayam@users.noreply.github.com>
2024-07-06 08:49:10 +0500

    Update Jackets.jsx

--- src/components/Jackets.jsx
+++ src/components/Jackets.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 const Jackets = () => {
   return (
     <div className='w-[95%] m-auto flex flex-col items-center gap-8'>
-        <Card imgSrc={'/jack1.jpg'} price={'250'} />
-        <Card imgSrc={'/jack2.jpg'} price={'250'} />
-        <Card imgSrc={'/jack3.jpg'} price={'250'} />
-        <Card imgSrc={'/jack4.jpg'} price={'250'} />
+        <Card imgSrc={'/jack1.jpg'} price={'250'} chez={'Jackets'} />
+        <Card imgSrc={'/jack2.jpg'} price={'250'} chez={'Jackets'} />
+        <Card imgSrc={'/jack3.jpg'} price={'250'} chez={'Jackets'} />
+        <Card imgSrc={'/jack4.jpg'} price={'250'} chez={'Jackets'} />
     </div>
   )
 }
